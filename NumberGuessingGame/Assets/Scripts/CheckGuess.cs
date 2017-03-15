cd82a06531423175b06820be69ad0154d27f3368
SueNBCMessingham <s.messingham@nbcol.ac.uk>
2017-03-15 21:11:51 +0000

    Add files via upload

---
+++ NumberGuessingGame/Assets/Scripts/CheckGuess.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+//class attached to Game Manager empty game object
+public class CheckGuess : MonoBehaviour {
+	//public properties, set in Editor
+	public InputField input;
+	public Text label;
+
+	private int RandomNumToGuess;
+	private int playerGuess;
+	// Use this for initialization
+	void Start () {
+		RandomNumToGuess = Random.Range(0, 100);
+	}
+	
+	public void CheckPlayerGuess(){
+		//convert text to int then store in playerGuess variable
+		playerGuess = int.Parse(input.text);
+
+		//conditional statement to check playerGuess against Random Num
+		if(playerGuess == RandomNumToGuess){
+			label.text = "Mind reader!! Correct number guessed";
+		} else if (playerGuess > RandomNumToGuess){
+			label.text = "Too high - try a lower number";
+		} else if (playerGuess < RandomNumToGuess) {
+			label.text = "Too low - try a higher number";
+		}
+
+		input.text = "";
+
+
+
+
+
+	}
+}
